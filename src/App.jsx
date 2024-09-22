549111b204ee46867f7b809c40b4ced416749dfc
Marvin <marvinraj77@gmail.com>
2024-09-23 05:29:37 +0800

    created appropriate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import TestComp from "./components/TestComp"
+import Navbar from "./components/Navbar"
+import Header from "./components/Header"
+import ImageCard from "./components/ImageCard"
+import Footer from "./components/Footer"
 
 function App() {
 
   return (
     <main>
-      <TestComp/>
+      <Navbar/>
+      <Header/>
+      <ImageCard/>
+      <Footer/>
     </main>
   )
 }

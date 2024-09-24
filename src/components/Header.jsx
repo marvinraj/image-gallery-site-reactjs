e3591e145ca179ca232fa0d4b1c8c18fd7e56067
Marvin <marvinraj77@gmail.com>
2024-09-25 01:50:53 +0800

    changed memories to memory

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <div className='header'>
-      <h1><span>my </span>MEMORIAS</h1>
+      <h1><span>my </span>MEMORY</h1>
       <p>a trip down memory lane to a time when everything was...</p>
     </div>
   )

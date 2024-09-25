eb176828889217e32e75ad370df0520ee6f523f4
Marvin <marvinraj77@gmail.com>
2024-09-26 03:47:31 +0800

    completed imagecard component

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -3,6 +3,14 @@ import image1 from "../assets/bird.jpg";
 import image2 from "../assets/views.jpg";
 import image3 from "../assets/paint.jpg";
 import image4 from "../assets/ship.jpg";
+import image5 from "../assets/ship.jpg";
+import image6 from "../assets/ship.jpg";
+import image7 from "../assets/ship.jpg";
+import image8 from "../assets/ship.jpg";
+import image9 from "../assets/ship.jpg";
+import image10 from "../assets/ship.jpg";
+import image11 from "../assets/ship.jpg";
+import image12 from "../assets/ship.jpg";
 
 export default function ImageCard() {
   // lets first create a list of objects
@@ -27,13 +35,53 @@ export default function ImageCard() {
       src: image4,
       alt: "ship",
     },
+    {
+      id: 5,
+      src: image5,
+      alt: "ship",
+    },
+    {
+      id: 6,
+      src: image6,
+      alt: "ship",
+    },
+    {
+      id: 7,
+      src: image4,
+      alt: "ship",
+    },
+    {
+      id: 8,
+      src: image5,
+      alt: "ship",
+    },
+    {
+      id: 9,
+      src: image6,
+      alt: "ship",
+    },
+    {
+      id: 10,
+      src: image4,
+      alt: "ship",
+    },
+    {
+      id: 11,
+      src: image5,
+      alt: "ship",
+    },
+    {
+      id: 12,
+      src: image6,
+      alt: "ship",
+    },
   ];
 
   return (
-    <div className='images'>
+    <section className='container images-grid'>
       {imagesList.map(img => (
           <img className='img-card' key={img.id} src={img.src} alt={img.alt}/>
         ))}
-    </div>
+    </section>
   )
 }

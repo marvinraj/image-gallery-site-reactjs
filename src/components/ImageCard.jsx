5cc86c37696f14df8cc84bfd9b79654e464235e1
Marvin <marvinraj77@gmail.com>
2024-09-25 21:31:42 +0800

    added 4 images for test run in imagecard.jsx

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import image1 from "../assets/bird.jpg";
+import image2 from "../assets/views.jpg";
+import image3 from "../assets/paint.jpg";
+import image4 from "../assets/ship.jpg";
 
 export default function ImageCard() {
+  // lets first create a list of objects
+  const imagesList = [
+    {
+      id: 1,
+      src: image1,
+      alt: "bird",
+    },
+    {
+      id: 2,
+      src: image2,
+      alt: "views",
+    },
+    {
+      id: 3,
+      src: image3,
+      alt: "paint",
+    },
+    {
+      id: 4,
+      src: image4,
+      alt: "ship",
+    },
+  ];
+
   return (
-    <div>
-      <img src={image1} alt={"a random bird"} />
+    <div className='images'>
+      {imagesList.map(img => (
+          <img className='img-card' key={img.id} src={img.src} alt={img.alt}/>
+        ))}
     </div>
   )
 }

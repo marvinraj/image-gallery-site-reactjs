29a7021151bd32f86df5191178fa87e367f9ee5f
Marvin <marvinraj77@gmail.com>
2024-09-26 19:19:22 +0800

    completed footer and added a tab icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div></div>
+    <div className='footer'>
+      <h3>who made this?</h3>
+      <h4>-- Marvin --</h4>
+    </div>
   )
 }
